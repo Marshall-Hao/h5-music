1043e6831f700fdd9bc9572589554a78ec59ce7a
Marshall Chan <marshallchan666@outlook.com>
2022-01-21 19:37:42 +0800

    initilize with H5 setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<m-header></m-header>
+</template>
+
+<script>
+	import Header from "./components/header/header.vue";
+
+	export default {
+		components: {
+			MHeader: Header,
+		},
+	};
+</script>
