b9f0a03161251e02c03683a6def05ad554858317
Marshall Chan <marshallchan666@outlook.com>
2022-01-22 00:56:08 +0800

    pic lazy load

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import lazyPlugin from "vue3-lazy";
 
 import "./assets/scss/index.scss";
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(lazyPlugin, {
+    loading: require("./assets/images/default.png"),
+  })
+  .mount("#app");
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -12,7 +12,7 @@
 					<ul>
 						<li v-for="item in albums" class="item" :key="item.id">
 							<div class="icon">
-								<img width="60" height="60" :src="item.pic" />
+								<img width="60" height="60" v-lazy="item.pic" />
 							</div>
 							<div class="text">
 								<h2 class="name">
